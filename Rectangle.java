e15ba778bb76abb459a9e1a0faf90b7de3ca7091
SaranSusruth <contactsusruth@gmail.com>
2025-06-21 18:00:09 +0530

    This is Rectangle class

---
+++ Rectangle.java
@@ -0,0 +1,21 @@
+public class Rectangle implements Shape
+{
+    private double width,height;
+    Rectangle(double width, double height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+    public double area()
+    {
+        return width * height;   
+    }
+    public double perimeter()
+    {
+        return 2 * (width * height);
+    }
+    public String getName()
+    {
+        return "Rectangle";
+    }
+}
